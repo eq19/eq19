bdd82a849c7af712f80e1550718b2458bef14c0c
eQuantum <8466209+eq19@users.noreply.github.com>
2024-11-03 14:51:36 +0700

    Update config.sh

--- .github/entrypoint/config.sh
+++ .github/entrypoint/config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-git config --global user.name "${{ inputs.actor }}"
-git config --global user.email "${{ inputs.actor }}@users.noreply.github.com"
+git config --global user.name "${{ github.actor }}"
+git config --global user.email "${{ github.actor }}@users.noreply.github.com"
 
 git config --global core.autocrlf false
 git config --global --add safe.directory "${{ github.workspace }}"       
