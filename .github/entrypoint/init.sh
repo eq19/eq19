868fdb10338ccb51fb6db0e5bc0df4239322b184
eQuantum <8466209+eq19@users.noreply.github.com>
2024-11-11 17:47:57 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -15,7 +15,7 @@ git config --global credential.helper store
 echo "https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com" > ~/.git-credentials
 
 TARGET_REPO="https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com/${TARGET_REPOSITORY}.git"
-REMOTE_REPO="https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com/${GITHUB_TEPOSITORY}.git"
+REMOTE_REPO="https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 API_URL="https://api.github.com/users/eq19/events/public"
 LATEST_COMMIT=$(curl -s $API_URL | jq ".[0].payload.commits[0].message")
