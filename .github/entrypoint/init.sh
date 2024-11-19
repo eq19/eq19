e5432240130b62045be38e4f77d8b5e37a3a576c
eQuantum <8466209+eq19@users.noreply.github.com>
2024-11-19 13:24:10 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -45,7 +45,7 @@ if [[ -z ${PASS} ]] || [[ "${PASS}" == "true" ]]; then
 
   if [[ -f _config.yml ]]; then
     mapfile -t ENV < <(yq '.[]' _config.yml)
-    declare -p ENV && echo "${ENV[@]}"
+    declare -p ENV && ls -al . && echo "${ENV[@]}"
   elif [[ -f /home/runner/_site/.env ]]; then
     set -a && . /home/runner/_site/.env && set +a
   fi
