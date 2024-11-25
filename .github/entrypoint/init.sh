f59031e8d014e007d280e28a7e2dde0a68dc3949
eQuantum <8466209+eq19@users.noreply.github.com>
2024-11-25 16:54:04 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -73,8 +73,8 @@ elif [[ "${JOB_ID}" == "4" ]]; then
   echo -e "\n${WIKI}"
   echo -e "\n${BASE}"
   echo -e "\n${FOLDER}"
-  echo -e "\n${RUNNER_TEMP}"
-  echo -e "\n${GITHUB_WORKSPACE}"
+  echo -e "\n${RUNNER_TEMP//\\//}"
+  echo -e "\n${GITHUB_WORKSPACE//\\//}"
   exit 1
 
   if [[ "${WIKI}" != "${BASE}" ]]; then
