575880dbe6208fac75e398ae3974a2842775d2ec
eQuantum <8466209+eq19@users.noreply.github.com>
2024-11-29 10:11:17 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -70,8 +70,8 @@ elif [[ "${JOB_ID}" == "3" ]]; then
 
 else
 
-  echo "https://${{ github.actor }}:${{ inputs.token }}@github.com/$TARGET_REPOSITORY.git"
   exit 1
+  echo "https://${{ github.actor }}:${{ inputs.token }}@github.com/$TARGET_REPOSITORY.git"
   git clone --single-branch --branch gh-source $TARGET_REPO ${RUNNER_TEMP//\\//}/gh-source
   cd ${RUNNER_TEMP//\\//}/gh-source && rm -rf .git .bundle && pwd && ls -al .
   
