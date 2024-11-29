c518673afe77372204fbcaab709e902e900198bc
eQuantum <8466209+eq19@users.noreply.github.com>
2024-11-29 10:49:14 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -72,13 +72,13 @@ else
 
   cd ${RUNNER_TEMP//\\//} && rm -rf gh-source
   git clone --single-branch --branch gh-source $TARGET_REPO gh-source
-  cd gh-source && rm -rf .git .bundle && pwd && ls -al .
-  exit 1
+  cd gh-source && rm -rf .git .bundle
   
   cd ${GITHUB_WORKSPACE//\\//}
   find -not -path "./.git/*" -not -name ".git" | grep git
   find -not -path "./.git/*" -not -name ".git" -delete
-  ls -al ${GITHUB_WORKSPACE//\\//}
+  pwd && ls -al ${GITHUB_WORKSPACE//\\//}
+  exit 1
   
   shopt -s dotglob
   mv -f ${RUNNER_TEMP//\\//}/gh-source/* . && cat _config.yml
