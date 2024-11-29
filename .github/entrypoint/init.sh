46874ef2e2bbf1f69f2a2ce427dcf83d801f115a
eQuantum <8466209+eq19@users.noreply.github.com>
2024-11-29 15:54:25 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -95,7 +95,6 @@ else
   #echo "action_state=yellow" | Out-File -FilePath $env:GITHUB_ENV -Append # no need for -Encoding utf8
   find . -iname '*.md' -print0 | sort -zn | xargs -0 -I '{}' front.sh '{}'
   cd ${GITHUB_WORKSPACE//\\//} && pwd && ls -alR .
-  exit 1
 
 fi
 
