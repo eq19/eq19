1acf36bf42faeca6cdbab0db2117e9e255307e79
eQuantum <8466209+eq19@users.noreply.github.com>
2024-11-30 21:30:33 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,6 @@
 
 #Ref: https://iree.dev/guides/ml-frameworks/tflite/#using-the-python-api
 
-import iree.compiler.tflite as iree_tflite_compile
-import iree.runtime as iree_rt
-import numpy
-import os
-import urllib.request
-
-from PIL import Image
-
-workdir = "/tmp/workdir"
-os.makedirs(workdir, exist_ok=True)
-
-tfliteFile = "/".join([workdir, "model.tflite"])
-jpgFile = "/".join([workdir, "input.jpg"])
-tfliteIR = "/".join([workdir, "tflite.mlir"])
-tosaIR = "/".join([workdir, "tosa.mlir"])
-bytecodeModule = "/".join([workdir, "iree.vmfb"])
-
-backends = ["llvm-cpu"]
-config = "local-task"
-
-tfliteUrl = "https://storage.googleapis.com/iree-model-artifacts/tflite-integration-tests/posenet_i8.tflite"
-jpgUrl = "https://storage.googleapis.com/iree-model-artifacts/tflite-integration-tests/posenet_i8_input.jpg"
-
-urllib.request.urlretrieve(tfliteUrl, tfliteFile)
-urllib.request.urlretrieve(jpgUrl, jpgFile)
+import tensorflow.compat.v2 as tf
+loaded_model = tf.saved_model.load('/path/to/downloaded/model/')
+print(list(loaded_model.signatures.keys()))
