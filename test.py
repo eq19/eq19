32f9d0277cbd690c27e66fccb9d32f8625365afd
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-01 11:27:27 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#Ref: https://iree.dev/guides/ml-frameworks/tflite/#using-the-python-api
+#Ref: https://github.com/iree-org/iree/tree/main/integrations/pjrt
 
 import jax
 a = jax.numpy.asarray([1, 2, 3, 4, 5, 6, 7, 8, 9])
