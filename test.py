a6d58b477b9b8e55f136337f6cdf089875ce9046
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-01 19:57:50 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #Ref: https://github.com/iree-org/iree/tree/main/integrations/pjrt
 
-import jax.numpy as np
-a = np.asarray([
+import jax.numpy as jnp
+a = jnp.array([
 			[0, 1, 0],
 			[1, 0, 0],
 			[0, 0, 0],
