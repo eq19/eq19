38fa67e75111ac572fd5885773ff5562d09ae983
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-02 10:32:15 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-#Ref: https://mathworld.wolfram.com/GeneralizedGell-MannMatrix.html
 #Ref: https://github.com/iree-org/iree/tree/main/integrations/pjrt
-
+#Ref: https://mathworld.wolfram.com/GeneralizedGell-MannMatrix.html
+#Ref: https://github.com/CQuIC/pysme/blob/master/src/pysme/gellmann.py
 import numpy as np
 a = np.array([
 			[0, 1, 0],
