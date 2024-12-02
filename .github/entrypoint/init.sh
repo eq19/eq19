3193ce229c2e0474b6f911d7a2da46e13d1aab6a
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-02 23:19:59 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -52,7 +52,7 @@ if [[ "${JOB_ID}" == "1" ]]; then
   git add . && git commit -m "update workflows" --quiet && git push --quiet
   if [ $? -eq 0 ]; then
     git clone --single-branch --branch gh-pages $REMOTE_REPO gh-pages && cd gh-pages
-    git add . && git commit --allow-empty -m "rerun due to job failure" && git push
+    git add . && git commit --allow-empty -m "rerun due to job update" && git push
     exit 1
   else
     ls -al ${GITHUB_WORKSPACE}
