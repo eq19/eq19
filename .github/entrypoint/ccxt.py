181209fee439aa24aae122231b97276d260afbf0
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-19 23:21:42 +0700

    Update ccxt.py

--- .github/entrypoint/ccxt.py
+++ .github/entrypoint/ccxt.py
@@ -5,21 +5,21 @@ import ccxt
 hitbtc   = ccxt.hitbtc({'verbose': True})
 bitmex   = ccxt.bitmex()
 huobipro = ccxt.huobipro()
-exmo     = ccxt.exmo({
+exmo     = ccxt.indodax({
     'apiKey': 'YOUR_PUBLIC_API_KEY',
     'secret': 'YOUR_SECRET_PRIVATE_KEY',
 })
-kraken = ccxt.kraken({
-    'apiKey': 'YOUR_PUBLIC_API_KEY',
-    'secret': 'YOUR_SECRET_PRIVATE_KEY',
-})
+#kraken = ccxt.kraken({
+    #'apiKey': 'YOUR_PUBLIC_API_KEY',
+    #'secret': 'YOUR_SECRET_PRIVATE_KEY',
+#})
 
-exchange_id = 'binance'
-exchange_class = getattr(ccxt, exchange_id)
-exchange = exchange_class({
-    'apiKey': 'YOUR_API_KEY',
-    'secret': 'YOUR_SECRET',
-})
+#exchange_id = 'binance'
+#exchange_class = getattr(ccxt, exchange_id)
+#exchange = exchange_class({
+    #'apiKey': 'YOUR_API_KEY',
+    #'secret': 'YOUR_SECRET',
+#})
 
 hitbtc_markets = hitbtc.load_markets()
 
@@ -34,10 +34,10 @@ print(huobipro.fetch_trades('LTC/USDT'))
 print(exmo.fetch_balance())
 
 # sell one ฿ for market price and receive $ right now
-print(exmo.id, exmo.create_market_sell_order('BTC/USD', 1))
+#print(exmo.id, exmo.create_market_sell_order('BTC/USD', 1))
 
 # limit buy BTC/EUR, you pay €2500 and receive ฿1  when the order is closed
-print(exmo.id, exmo.create_limit_buy_order('BTC/EUR', 1, 2500.00))
+#print(exmo.id, exmo.create_limit_buy_order('BTC/EUR', 1, 2500.00))
 
 # pass/redefine custom exchange-specific order params: type, amount, price, flags, etc...
-kraken.create_market_buy_order('BTC/USD', 1, {'trading_agreement': 'agree'})
+#kraken.create_market_buy_order('BTC/USD', 1, {'trading_agreement': 'agree'})
