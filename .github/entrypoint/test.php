ff830eb0efd7467175d7c1d6722df7df6c6313d3
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-19 22:06:45 +0700

    Update test.php

--- .github/entrypoint/test.php
+++ .github/entrypoint/test.php
@@ -3,28 +3,5 @@
     $key = '${ACCESS_API}';
     $secretKey = '{ACCESS_KEY}';
     
-	$data = [
-        'method' => 'transHistory',
-        'timestamp' => '1578304294000',
-        'recvWindow' => '1578303937000'
-    ];
-	$post_data = http_build_query($data, '', '&');
-    $sign = hash_hmac('sha512', $post_data, $secretKey);
-
-    $headers = ['Key:'.$key,'Sign:'.$sign];
-
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-        CURLOPT_HTTPHEADER => $headers,
-        CURLOPT_URL => $url,
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => $data,
-        CURLOPT_RETURNTRANSFER => true
-    ));
-
-    $response = curl_exec($curl);
-
-    curl_close($curl);
-    echo $response;
+phpinfo(INFO_MODULES);
 ?>
