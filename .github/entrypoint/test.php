2ed69f8ed516728aa60113195025e643645478d3
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-19 21:47:09 +0700

    Update test.php

--- .github/entrypoint/test.php
+++ .github/entrypoint/test.php
@@ -4,14 +4,9 @@
     $secretKey = '{ACCESS_KEY}';
     
 	$data = [
-        'method' => 'trade',
+        'method' => 'transHistory',
         'timestamp' => '1578304294000',
-        'recvWindow' => '1578303937000',
-        'pair' => 'btc_idr',
-        'type' => 'sell',
-        'price' => '1700000000',
-        'idr' => '',
-        'btc' => '0.00002973'
+        'recvWindow' => '1578303937000'
     ];
 	$post_data = http_build_query($data, '', '&');
     $sign = hash_hmac('sha512', $post_data, $secretKey);
