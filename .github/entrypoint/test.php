9d1cfb2184f51bd57c6d3a4dfb19a3512efa7b57
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-19 21:29:25 +0700

    Update test.php

--- .github/entrypoint/test.php
+++ .github/entrypoint/test.php
@@ -11,7 +11,7 @@
         'type' => 'sell',
         'price' => '1700000000',
         'idr' => '',
-        'btc' => '0.00000863'
+        'btc' => '0.00002973'
     ];
 	$post_data = http_build_query($data, '', '&');
     $sign = hash_hmac('sha512', $post_data, $secretKey);
