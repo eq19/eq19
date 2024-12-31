ecfa44c9eaedbe07881be695a939b99eefa2a64e
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-31 22:24:02 +0700

    Update startup.sh

--- .github/entrypoint/startup.sh
+++ .github/entrypoint/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Fetch the config.json from Google Secret Manager
-gcloud secrets versions access latest --secret=freqtrade-config > /home/runner/config.json
+/mnt/disks/deeplearning/usr/bin/gcloud secrets versions access latest --secret=freqtrade-config > /home/runner/config.json
 
 # Start Freqtrade
 freqtrade trade --config /home/runner/config.json
