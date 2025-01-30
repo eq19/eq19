dc4c7aa58f7008022b39fa0d4a237e92ad9d36e3
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-31 06:50:37 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -82,10 +82,6 @@ elif [[ "${JOB_ID}" == "3" ]]; then
   sed -i 's/0. \[\[//g' ${RUNNER_TEMP}/Sidebar.md && sed -i 's/\]\]//g' ${RUNNER_TEMP}/Sidebar.md
   cd /home/runner/_site && cp -R ${RUNNER_TEMP}/gistdir/* . && ls -lR .
 
-  # Bring GitHub Env Variables in to the config file on another container usong docker from the attached deeplearning image
-  /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|your_exchange_key|${ACCESS_API}|g" /home/runner/config.json
-  /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|your_exchange_secret|${ACCESS_KEY}|g" /home/runner/config.json
-
 else
 
   cd ${RUNNER_TEMP//\\//} && rm -rf gh-source
