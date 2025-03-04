f63bfeb4725fb66ad84f7894fb8f03d5d7324995
eQuantum <8466209+eq19@users.noreply.github.com>
2025-03-05 01:40:31 +0700

    Update github_pages.sh

--- .github/entrypoint/github_pages.sh
+++ .github/entrypoint/github_pages.sh
@@ -10,11 +10,9 @@ if [[ "${WIKI}" != "${BASE}" ]]; then
   cd ${RUNNER_TEMP}/wikidir && mv -f Home.md README.md
 
   find . -type d -name "${FOLDER}" -prune -exec sh -c 'wiki.sh "$1"' sh {} \;
-  find . -type d -name "${FOLDER}" -prune -exec sh -c 'cat ${RUNNER_TEMP//\\//}/README.md >> $1/README.md' sh {} \;
-  find . -type d -name "${FOLDER}" -prune -exec sh -c 'ls -alR' sh {} \;
+  find . -type d -name "${FOLDER}" -prune -exec sh -c 'cat ${RUNNER_TEMP}/README.md >> $1/README.md' sh {} \;
 
 fi
    
-#echo "action_state=yellow" | Out-File -FilePath $env:GITHUB_ENV -Append # no need for -Encoding utf8
 find . -iname '*.md' -print0 | sort -zn | xargs -0 -I '{}' front.sh '{}'
 cd ${GITHUB_WORKSPACE} && pwd && ls -alR .
