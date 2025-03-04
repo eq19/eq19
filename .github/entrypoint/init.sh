d23aef6c2c90334184d89c1f6d1998b7d3ef3440
eQuantum <8466209+eq19@users.noreply.github.com>
2025-03-04 17:11:27 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -90,7 +90,6 @@ else
   git clone --single-branch --branch gh-source $TARGET_REPO gh-source
   
   cd ${GITHUB_WORKSPACE//\\//}
-  #find -not -path "./.git/*" -not -name ".git" | grep git
   find -not -path "./.git/*" -not -name ".git" -delete
 
   rm -rf ${RUNNER_TEMP//\\//}/gh-source/.git
