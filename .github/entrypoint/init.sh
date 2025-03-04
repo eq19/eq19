9295ea76bec405c4b36684a6c9b1bb4b198941c6
eQuantum <8466209+eq19@users.noreply.github.com>
2025-03-05 00:20:56 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -78,9 +78,8 @@ elif [[ "${JOB_ID}" == "3" ]]; then
   gist.sh ${TARGET_REPOSITORY} ${FOLDER}
 
   find ${RUNNER_TEMP}/gistdir -type d -name .git -prune -exec rm -rf {} \;
-  mv -f ${RUNNER_TEMP}/workdir/* /home/runner/_site/
-
   rm -rf ${RUNNER_TEMP}/Sidebar.md && cp _Sidebar.md ${RUNNER_TEMP}/Sidebar.md
+
   sed -i 's/0. \[\[//g' ${RUNNER_TEMP}/Sidebar.md && sed -i 's/\]\]//g' ${RUNNER_TEMP}/Sidebar.md
   cd /home/runner/_site && cp -R ${RUNNER_TEMP}/gistdir/* . && github_pages.sh && ls -lR .
 
