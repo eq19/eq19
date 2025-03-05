c96627e2fd32d681730b3f0e856d031e81315295
eQuantum <8466209+eq19@users.noreply.github.com>
2025-03-05 11:19:35 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -75,10 +75,11 @@ elif [[ "${JOB_ID}" == "2" ]]; then
 elif [[ "${JOB_ID}" == "3" ]]; then
 
   cd /home/runner/_site && cat _config.yml
-  gist.sh ${BASE} && cp -R ${RUNNER_TEMP}/wikidir/* .
+  gist.sh ${BASE} > /dev/null && cat ${RUNNER_TEMP}/spin.txt
+  cp -R ${RUNNER_TEMP}/wikidir/* .
 
   if [[ "${WIKI}" != "${BASE}" ]]; then
-    gist.sh ${WIKI}
+    gist.sh ${WIKI} > /dev/null && cat ${RUNNER_TEMP}/spin.txt
     find . -type d -name "${FOLDER}" -prune -exec sh -c 'cp -R ${RUNNER_TEMP}/wikidir/* "$1/"' sh {} \;
   fi
 
