3be3f9df88a69472a0d567a7ce3af4c6caa9bf61
eQuantum <8466209+eq19@users.noreply.github.com>
2025-03-09 05:42:17 +0700

    Update sync.sh

--- .github/entrypoint/sync.sh
+++ .github/entrypoint/sync.sh
@@ -61,8 +61,8 @@ sync_secrets_and_variables() {
             echo "➕ Secret '$secret' does not exist in $target_repo. Copying..."
  
             [[ "$secret" == "ACCESS_TOKEN" ]] && secret_value=$GITHUB_PAT
-            [[ "$secret" == "ACCESS_TOKEN" ]] && secret_value=$GITHUB_PAT
-            [[ "$secret" == "ACCESS_TOKEN" ]] && secret_value=$GITHUB_PAT
+            [[ "$secret" == "DOCKER_HUB_TOKEN" ]] && secret_value=$DOCKER_HUB_TOKEN
+            [[ "$secret" == "DOCKER_HUB_PASSWORD" ]] && secret_value=$DOCKER_HUB_PASSWORD
              
             set_secret "$target_repo" "$secret" "$secret_value"
         else
