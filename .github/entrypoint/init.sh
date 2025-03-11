8b9be4455045389d54bc5a476d145d01a25f0684
eQuantum <8466209+eq19@users.noreply.github.com>
2025-03-11 09:42:11 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -62,8 +62,12 @@ if [[ "${JOB_ID}" == "1" ]]; then
     git add . && git commit --allow-empty -m "rerun due to job update" && git push
     exit 1
   else
-    PARAMS=.github/entrypoint/artifact/python/src/params/spaces.json
-    mv -f ${GITHUB_WORKSPACE}/$PARAMS $1/dataFile/user_data/strategies/fibbo.json
+    #PARAMS=.github/entrypoint/artifact/python/src/params/spaces.json
+    #mv -f ${GITHUB_WORKSPACE}/$PARAMS $1/dataFile/user_data/strategies/fibbo.json
+    curl -s -H "Authorization: token $GITHUB_PAT" \
+      -H "Accept: application/vnd.github.v3+json" \
+      "$GITHUB_API/repos/$source_repo/actions/variables/$var" \
+      | jq -r '.value' > $1/dataFile/user_data/strategies/fibbo.json
   
     mv -f $1/dataFile/user_data ${GITHUB_WORKSPACE}/
     cd ${GITHUB_WORKSPACE} && ls -al ${GITHUB_WORKSPACE}
