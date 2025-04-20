c490df0e097f2af4afbf983c6c16036470da54f8
eQuantum <8466209+eq19@users.noreply.github.com>
2025-04-21 05:54:56 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -47,9 +47,6 @@ if [[ -z ${PASS} ]] || [[ "${PASS}" == "true" ]]; then
 fi
 
 echo -e "\n$hr\nWORKSPACE\n$hr"
-RERUN_RUNNER=$(curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
-  "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/RERUN_RUNNER" | jq -r '.value')
-
 if [[ "${JOBS_ID}" == "1" ]]; then
 
   cd ${GITHUB_WORKSPACE} && rm -rf .github
@@ -96,6 +93,10 @@ if [[ "${JOBS_ID}" == "1" ]]; then
 elif [[ "${JOBS_ID}" == "2" ]]; then
 
   ls -alR ${GITHUB_WORKSPACE}
+  
+  RERUN_RUNNER=$(curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
+    "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/RERUN_RUNNER" | jq -r '.value')
+
   echo 'RERUN_RUNNER='${RERUN_RUNNER} >> ${GITHUB_ENV}
 
 elif [[ "${JOBS_ID}" == "3" ]]; then
