d5b20e5bc8b41cdf916a7ef61407af0c7b237a79
eQuantum <8466209+eq19@users.noreply.github.com>
2025-04-20 23:14:53 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -91,7 +91,6 @@ if [[ "${JOBS_ID}" == "1" ]]; then
 
     cd ${GITHUB_WORKSPACE}
     rm -rf user_data && mv -f $1/user_data . && ls -al .
-    if [[ "${RERUN_RUNNER}" != "false" ]]; then gh variable set RERUN_RUNNER --body "false"; fi
 
   fi
 
