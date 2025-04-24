1d9b4bc5e98da294a717ea4e7ac3ed4ec3b6ebff
eQuantum <8466209+eq19@users.noreply.github.com>
2025-04-24 22:15:22 +0700

    Update exitpoint.sh

--- .github/entrypoint/dockerfile/scripts/exitpoint.sh
+++ .github/entrypoint/dockerfile/scripts/exitpoint.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-cd /home/runner/_site
+SCOPE="repos"
+cd /home/runner
+RUNNER_URL="https://github.com/$1"
 
 if [[ -z $RUNNER_TOKEN && -z $GITHUB_ACCESS_TOKEN ]]; then
     echo "Error : You need to set RUNNER_TOKEN (or GITHUB_ACCESS_TOKEN) environment variable."
     exit 1
+fi
+
+if [[ -z $RUNNER_NAME ]]; then
+    echo "RUNNER_NAME environment variable is not set, using '${HOSTNAME}'."
+    export RUNNER_NAME=${HOSTNAME}
+fi
+
+if [[ -z $RUNNER_WORK_DIRECTORY ]]; then
+    echo "RUNNER_WORK_DIRECTORY environment variable is not set, using '_work'."
+    export RUNNER_WORK_DIRECTORY="_work"
 fi
 
 if [[ -z $RUNNER_REPLACE_EXISTING ]]; then
@@ -13,51 +25,38 @@ fi
 
 CONFIG_OPTS=""
 if [ "$(echo $RUNNER_REPLACE_EXISTING | tr '[:upper:]' '[:lower:]')" == "true" ]; then
-	CONFIG_OPTS="--replace"
+    CONFIG_OPTS="--replace"
 fi
 
 if [[ -n $RUNNER_LABELS ]]; then
     CONFIG_OPTS="${CONFIG_OPTS} --labels ${RUNNER_LABELS}"
 fi
 
-if [[ -f $GITHUB_WORKSPACE/_config.yml ]]; then
-    FOLDER=$(yq '.span' $GITHUB_WORKSPACE/_config.yml)
-    export RUNNER_NAME=$(eval echo $FOLDER)
-    export RUNNER_WORK_DIRECTORY=$(eval echo $FOLDER)
+if [[ -f /home/runner/config.sh ]]; then
 
-    TARGET_REPOSITORY=$(yq '.repository' $GITHUB_WORKSPACE/_config.yml)
-    if [[ "$TARGET_REPOSITORY" != *"eq19/"* ]]; then
-        SCOPE="orgs"
-        RUNNER_URL="https://github.com/${TARGET_REPOSITORY%%/*}"
-    else
-        SCOPE="repos"
-        RUNNER_URL="https://github.com/${TARGET_REPOSITORY}"
-    fi
+    echo "Exchanging the GitHub Access Token with a Runner Token (scope: ${SCOPE})..."
+    
+    _PROTO="$(echo "${RUNNER_URL}" | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    _URL="$(echo "${RUNNER_URL/${_PROTO}/}")"
+    _PATH="$(echo "${_URL}" | grep / | cut -d/ -f2-)"
 
-    if [[ -n $GITHUB_ACCESS_TOKEN ]]; then
+    RUNNER_TOKEN="$(curl -XPOST -fsSL \
+        -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
+        -H "Accept: application/vnd.github.v3+json" \
+        "https://api.github.com/${SCOPE}/${_PATH}/actions/runners/registration-token" \
+        | jq -r '.token')"
 
-        echo "Exchanging the GitHub Access Token with a Runner Token (scope: ${SCOPE})..."
-
-        _PROTO="$(echo "${RUNNER_URL}" | grep :// | sed -e's,^\(.*://\).*,\1,g')"
-        _URL="$(echo "${RUNNER_URL/${_PROTO}/}")"
-        _PATH="$(echo "${_URL}" | grep / | cut -d/ -f2-)"
-
-        RUNNER_TOKEN="$(curl -XPOST -fsSL \
-            -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
-            -H "Accept: application/vnd.github.v3+json" \
-            "https://api.github.com/${SCOPE}/${_PATH}/actions/runners/registration-token" \
-            | jq -r '.token')"
-    fi
-
-    # Register new URL
-    ../config.sh \
+    # Register new URL    
+    ./svc.sh stop
+    ./svc.sh uninstall
+    ./config.sh \
         --url $RUNNER_URL \
         --token $RUNNER_TOKEN \
         --name $RUNNER_NAME \
         --work $RUNNER_WORK_DIRECTORY \
         $CONFIG_OPTS \
         --unattended
-    ../svc.sh install
-    ../svc.sh start
+    ./svc.sh install
+    ./svc.sh start
 
 fi
