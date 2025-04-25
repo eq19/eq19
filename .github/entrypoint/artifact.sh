db62053325ec1cadc76c7df853a1566283ab3634
eQuantum <8466209+eq19@users.noreply.github.com>
2025-04-25 08:08:37 +0700

    Update artifact.sh

--- .github/entrypoint/artifact.sh
+++ .github/entrypoint/artifact.sh
@@ -100,9 +100,11 @@ jekyll_build() {
   echo 'repo='${TARGET_REPOSITORY} >> ${GITHUB_OUTPUT}
   echo 'TARGET_REPOSITORY='${TARGET_REPOSITORY} >> ${GITHUB_ENV}
 
-  NEXT_REPOSITORY=$(next_repo "${TARGET_REPOSITORY}")
-  gh variable set TARGET_REPOSITORY --body "$NEXT_REPOSITORY" -repos $TARGET_REPOSITORY
-  
+  if [[ "${TARGET_REPOSITORY}" != *"eq19/"* ]]; then
+    NEXT_REPOSITORY=$(next_repo "${TARGET_REPOSITORY}")
+    gh variable set TARGET_REPOSITORY --body "$NEXT_REPOSITORY" -repos $TARGET_REPOSITORY
+  fi
+
   sed -i "1s|^|title: eQuantum\n|" ${RUNNER_TEMP}/_config.yml
   sed -i "1s|^|span: ${FOLDER}\n|" ${RUNNER_TEMP}/_config.yml
   sed -i "1s|^|user: ${USER}\n|" ${RUNNER_TEMP}/_config.yml
