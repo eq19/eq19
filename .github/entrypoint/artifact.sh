a80839680b82bbeb64aa05c077b2df8bb0a15ce9
eQuantum <8466209+eq19@users.noreply.github.com>
2025-04-25 13:44:20 +0700

    Update artifact.sh

--- .github/entrypoint/artifact.sh
+++ .github/entrypoint/artifact.sh
@@ -137,7 +137,6 @@ jekyll_build() {
 
 # Define the next repository function using jq
 next_repo() {
-
   local target_repo="$1"
   jq -r --arg target "$target_repo" '
     ($target | split("/")) as $parts |
@@ -149,6 +148,7 @@ next_repo() {
       "Organization not found: \($org)" | halt_error(1)
     else 
       .[$org_index] as $current_org |
+      (.[($org_index + 1) % length].login) as $next_org |
 
       if $repo == "\($org).github.io" then
         "\($org)/\($current_org.key1[0])"
@@ -166,7 +166,7 @@ next_repo() {
             if ($key2_index + 1) < ($current_org.key2 | length) then
               "\($org)/\($current_org.key2[$key2_index + 1])"
             else
-              "\($org)/\($org).github.io"
+              "\($next_org)/\($next_org).github.io"
             end
           else
             "Repository not found: \($repo)" | halt_error(1)
