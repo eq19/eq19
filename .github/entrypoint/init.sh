5240b39d9658e6116ed7bcbf20bbbd2d8ee41cf7
eQuantum <8466209+eq19@users.noreply.github.com>
2025-04-26 11:24:20 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -50,7 +50,6 @@ if [[ -z ${PASS} ]] || [[ "${PASS}" == "true" ]]; then
   elif [[ -f /home/runner/_site/.env ]]; then
     set -a && . /home/runner/_site/.env && set +a
   fi
-  export PATH=.github/entrypoint:$PATH    
   printenv | sort
 
 fi
