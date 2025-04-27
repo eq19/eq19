0a6684e2492f931e79bcc058596e543e1cf00873
eQuantum <8466209+eq19@users.noreply.github.com>
2025-04-27 22:18:48 +0700

    Update sync.sh

--- .github/entrypoint/sync.sh
+++ .github/entrypoint/sync.sh
@@ -57,8 +57,10 @@ sync_secrets_and_variables() {
         -H "Accept: application/vnd.github.v3+json" "$GITHUB_API/repos/$source_repo/actions/secrets" | jq -r '.secrets[].name')
 
     for secret in $secrets; do
-       set_secret "$target_repo" "$secret" "${!secret}"
-       #if [[ "$(check_secret_exists "$target_repo" "$secret")" == "404" ]]; then
+        secret_value=$(curl -s -H "Authorization: token $GITHUB_PAT" \
+            -H "Accept: application/vnd.github.v3+json" "$GITHUB_API/repos/$source_repo/actions/secrets/$secret" | jq -r '.value')
+        set_secret "$target_repo" "$secret" "$secret_value"
+        #if [[ "$(check_secret_exists "$target_repo" "$secret")" == "404" ]]; then
             #echo "➕ Secret '$secret' does not exist in $target_repo. Copying..."
             #set_secret "$target_repo" "$secret" "${!secret}"
         #else
