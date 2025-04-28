a8bfa1c339695eb9a8e668244f6b1aad7ced5947
eQuantum <8466209+eq19@users.noreply.github.com>
2025-04-28 22:07:14 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -14,26 +14,6 @@ git config --global --add safe.directory "${GITHUB_WORKSPACE}"
 git config --global credential.helper store
 echo "https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com" > ~/.git-credentials
 
-# Loop through all repos and cancel runs except current one
-ALL_REPOS=""
-ORGS=$(gh api user/orgs --jq '.[].login')
-CURRENT_REPO=$(gh repo view --json nameWithOwner -q '.nameWithOwner')
-ALL_REPOS+=$(gh repo list --limit 1000 --json nameWithOwner -q '.[].nameWithOwner')
-
-# Get organization repositories for each org
-for org in $ORGS; do
-  echo "Fetching repos for organization: $org"
-  ALL_REPOS+=$(gh repo list $org --limit 1000 --json nameWithOwner -q '.[].nameWithOwner')
-done
-
-for REPO in $ALL_REPOS; do
-  if [ "$REPO" != "$CURRENT_REPO" ]; then
-    RUNS=$(gh api "repos/$REPO/actions/runs?status=in_progress" --jq '.workflow_runs[].id')
-    RUNS+=" $(gh api "repos/$REPO/actions/runs?status=queued" --jq '.workflow_runs[].id')"
-    #for RUN_ID in $RUNS; do gh api -X POST "repos/$REPO/actions/runs/$RUN_ID/force-cancel"; done
-  fi
-done
-
 export RERUN_RUNNER=$(curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
   "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/RERUN_RUNNER" | jq -r '.value')
 export TARGET_REPOSITORY=$(curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
