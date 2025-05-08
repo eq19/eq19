91276722115b9366d7904770c28aa5bd4ccb6007
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-08 09:12:07 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -87,6 +87,14 @@ if [[ "${JOBS_ID}" == "1" ]]; then
   else
 
     if [[ ! -f $RUNNER_TEMP/_config.yml ]]; then set_config $1; fi
+    if [[ "$(yq '.span' _config.yml)" != "$TARGET_REPOSITORY" ]]; then
+      curl -s -X POST \
+        -H "Authorization: token $GH_TOKEN" \
+        -H "Accept: application/vnd.github.v3+json" \
+        "https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches" \
+        -d '{"event_type": "retry_workflow", "client_payload": {"original_run_id": "${GITHUB_RUN_ID}"}}'
+      exit 1
+    fi
 
     cd $1 && javac -d user_data/ft_client/test_client javaCode/Main.java
     cd $GITHUB_WORKSPACE && rm -rf user_data && mv -f $1/user_data .
