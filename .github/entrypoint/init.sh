9d8519c372a3e0b18d51f2e12f13bba1ea3af95f
eQuantum <8466209+eq19@users.noreply.github.com>
2025-05-22 20:52:57 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -94,8 +94,9 @@ if [[ "${JOBS_ID}" == "1" ]]; then
     fi
 
     #Ref: https://github.com/tsoding/JelloVM
-    gcc -Wall -Wextra $1/gccCode/src/decoder.c -o float_decoder
-    cd $1 && javac -d user_data/ft_client/test_client javaCode/Main.java
+    cd $1 && mv -f pythonCode user_data/ft_client/test_client && \
+    gcc -Wall -Wextra gccCode/src/decoder.c -o float_decoder && \
+    javac -d user_data/ft_client/test_client javaCode/Main.java
 
     cd $GITHUB_WORKSPACE && rm -rf user_data && mv -f $1/user_data .
     echo -e "\n$hr\nWORKSPACE\n$hr" && ls -al .
