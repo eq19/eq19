e64f3302cf5da65ec6d781bdd1b6c67fa5bede3a
eQuantum <8466209+eq19@users.noreply.github.com>
2025-06-02 00:34:42 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -91,6 +91,15 @@ if [[ "${JOBS_ID}" == "1" ]]; then
         "https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches" \
         -d '{"event_type": "retry_workflow", "client_payload": {"original_run_id": "${GITHUB_RUN_ID}"}}'
       exit 1
+    else
+      HEADER="Accept: application/vnd.github+json"
+      RESPONSE=$(gh api -H "${HEADER}" repos/$TARGET_REPOSITORY/actions/runners)
+      STATUS=$(echo "$RESPONSE" | jq -r --arg NAME "$RUNNER_TITLE" '.runners[] | select(.name == $NAME).status')
+
+      if [[ "$STATUS" == "offline" ]]; then
+        RUNNER_ID=$(gh api -H "${HEADER}" /repos/$TARGET_REPOSITORY/actions/runners --jq '.runners.[].id')
+        gh api --method DELETE -H "${HEADER}" /repos/$TARGET_REPOSITORY/actions/runners/${RUNNER_ID}
+      fi
     fi
 
     cd $GITHUB_WORKSPACE
