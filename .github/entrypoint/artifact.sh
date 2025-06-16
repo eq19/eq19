d161d4f0696f57025eeb739d8bb748578bf74f75
eQuantum <8466209+eq19@users.noreply.github.com>
2025-06-16 11:46:33 +0700

    Update artifact.sh

--- .github/entrypoint/artifact.sh
+++ .github/entrypoint/artifact.sh
@@ -33,6 +33,7 @@ set_target() {
   
   # Iterate the Structure
   printf -v array_str -- ',,%q' "${array[@]}"
+  echo $array_str
   if [[ ! "${array_str},," =~ ",,$1,," ]]; then
     SPAN=0; echo ${array[0]}
   elif [[ "${array[-1]}" == "$1" ]]; then
