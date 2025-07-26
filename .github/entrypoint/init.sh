ac7b6fa7cb26474607477ee4ef3dbd4a3131da2f
eQuantum <8466209+eq19@users.noreply.github.com>
2025-07-26 22:10:03 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -192,8 +192,6 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
   fi
 
   # Get the strategy file and params value then save to fibbo.py and fibbo.json
-  cp /home/runner/user_data/strategies/__init__.py /home/runner/data_dry/strategies/__init__.py 
-  cp /home/runner/user_data/strategies/__init__.py /home/runner/data_live/strategies/__init__.py 
   cp /home/runner/user_data/strategies/hyperopt_params.json /home/runner/data_dry/strategies/hyperopt_params.json
   cp /home/runner/user_data/strategies/hyperopt_params.json /home/runner/data_live/strategies/hyperopt_params.json
 
