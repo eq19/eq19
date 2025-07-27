e1ab199dd2407baf93739116a003f20fca96ddf5
eQuantum <8466209+eq19@users.noreply.github.com>
2025-07-28 06:05:13 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -223,6 +223,9 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
     /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|your_telegram_chat_id|$TELEGRAM_CHAT_ID|g" $CONFIG
     /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|config_examples|/home/runner/user_data/config_examples|g" $CONFIG
 
+    /mnt/disks/deeplearning/usr/bin/docker exec mydb ls -alR /home/runner/data_dry
+    /mnt/disks/deeplearning/usr/bin/docker exec mydb ls -alR /home/runner/data_live
+
     /mnt/disks/deeplearning/usr/bin/docker exec mydb jq '.telegram.enabled = true' $CONFIG > $CONFIG_DRY
     /mnt/disks/deeplearning/usr/bin/docker exec mydb jq '.telegram.enabled = true | .dry_run = false' $CONFIG > $CONFIG_LIVE
 
@@ -261,8 +264,6 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
 
   /mnt/disks/deeplearning/usr/bin/docker exec mydb cp $HYPEROPT_PARAM /home/runner/data_dry/strategies/hyperopt_params.json
   /mnt/disks/deeplearning/usr/bin/docker exec mydb cp $HYPEROPT_PARAM /home/runner/data_live/strategies/hyperopt_params.json
-  /mnt/disks/deeplearning/usr/bin/docker exec mydb ls -alR /home/runner/data_dry
-  /mnt/disks/deeplearning/usr/bin/docker exec mydb ls -alR /home/runner/data_live
 
   echo -e "\n$hr\nCONFIG\n$hr" && cat _config.yml
   echo -e "\n$hr\nENVIRONTMENT\n$hr" && printenv | sort
