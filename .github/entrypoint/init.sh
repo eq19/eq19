8eed0c0319e2f1b1f40b68a15521eac2ef50a75d
eQuantum <8466209+eq19@users.noreply.github.com>
2025-07-29 10:11:49 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -224,6 +224,7 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
   CONFIG="/home/runner/user_data/config.json"
   CONFIG_DRY="/home/runner/data_dry/config.json"
   CONFIG_LIVE="/home/runner/data_live/config.json"
+  HYPEROPT_BASE="/home/runner/user_data/strategies/orgs.json"
   CONFIG_BASE="$BASE_URL/config_examples/config_exchange.example.json"
   HYPEROPT_PARAM="/home/runner/user_data/strategies/hyperopt_params.json"
 
