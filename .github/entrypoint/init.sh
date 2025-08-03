e0878c5abd7a0c1ea5d415e17086b09a2f7dc23b
eQuantum <8466209+eq19@users.noreply.github.com>
2025-08-03 18:43:43 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -224,12 +224,14 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
   CONFIG="/home/runner/user_data/config.json"
   CONFIG_DRY="/home/runner/data_dry/config.json"
   CONFIG_LIVE="/home/runner/data_live/config.json"
+  CONFIG_FULL="$BASE_URL/config_examples/config_full.example.json"
   CONFIG_BASE="$BASE_URL/config_examples/config_exchange.example.json"
   HYPEROPT_PARAM="/home/runner/user_data/strategies/hyperopt_params.json"
+  EXCHANGE_PARAM="/home/runner/user_data/config_examples/config_exchange.example.json"
 
   set -euo pipefail  # Strict error handling
   /mnt/disks/deeplearning/usr/bin/docker exec mydb rm -rf "$CONFIG"
-  if /mnt/disks/deeplearning/usr/bin/docker exec mydb curl -sf -o "$CONFIG" "$CONFIG_BASE"; then
+  if /mnt/disks/deeplearning/usr/bin/docker exec mydb curl -sf -o "$CONFIG" "$CONFIG_FULL"; then
     /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|your_telegram_chat_id|$TELEGRAM_CHAT_ID|g" $CONFIG
     /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|config_examples|/home/runner/user_data/config_examples|g" $CONFIG
 
