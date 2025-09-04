59fa4d5e5877ac1c59facc5dafc389b14c6f159d
eQuantum <8466209+eq19@users.noreply.github.com>
2025-09-04 17:37:57 +0700

    Update job_started.sh

--- .github/entrypoint/dockerfile/hooks/job_started.sh
+++ .github/entrypoint/dockerfile/hooks/job_started.sh
@@ -2,7 +2,7 @@
 # Structure: Cell Types – Modulo 6
 # Action https://github.com/${REPO}/actions/runs/${RUN}
 
-hr='------------------------------------------------------------------------------------'
+hr='----------------------------------------------------------------------------------'
 
 echo -e "\n$hr\nGroups\n$hr"
 getent group
