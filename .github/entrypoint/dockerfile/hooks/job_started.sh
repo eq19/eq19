e4482476f693d093a9aeaf0280d3166294b87b11
eQuantum <8466209+eq19@users.noreply.github.com>
2025-09-04 17:23:09 +0700

    Update job_started.sh

--- .github/entrypoint/dockerfile/hooks/job_started.sh
+++ .github/entrypoint/dockerfile/hooks/job_started.sh
@@ -99,24 +99,47 @@ if [ -d /mnt/disks/deeplearning/usr/local/sbin ]; then
   #echo -e "\n$hr\nDeepLearning Locate Terraform\n$hr" 
   #find /mnt/disks/deeplearning -type d -name '*terraform*' | sort
 
-  # Setup freqtrade userdir
-  if /mnt/disks/deeplearning/usr/bin/docker ps --format '{{.Names}}' | grep -q "^mydb$"; then
+  # Max retries
+  max_retries=10
+  # Interval between checks (10 retries in 10 minutes -> 60s each)
+  interval=60
 
-    if ! /mnt/disks/deeplearning/usr/bin/docker exec mydb [ -d "/home/runner/data_dry" ]; then
-      /mnt/disks/deeplearning/usr/bin/docker exec mydb freqtrade create-userdir --userdir /home/runner/data_dry
-      /mnt/disks/deeplearning/usr/bin/docker exec mydb mkdir -p /home/runner/data_dry/strategies/utils
-    elif /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl status freqtrade_dry | grep -q "RUNNING"; then
-      /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl stop freqtrade_dry
+  for ((i=1; i<=max_retries; i++)); do
+    echo "Check $i of $max_retries..."
+
+    if /mnt/disks/deeplearning/usr/bin/docker ps --format '{{.Names}}' | grep -wq "^mydb$"; then
+      echo "Condition fulfilled ✅"
+
+        # Setup freqtrade userdir
+        if /mnt/disks/deeplearning/usr/bin/docker ps --format '{{.Names}}' | grep -q "^mydb$"; then
+
+          if ! /mnt/disks/deeplearning/usr/bin/docker exec mydb [ -d "/home/runner/data_dry" ]; then
+            /mnt/disks/deeplearning/usr/bin/docker exec mydb freqtrade create-userdir --userdir /home/runner/data_dry
+            /mnt/disks/deeplearning/usr/bin/docker exec mydb mkdir -p /home/runner/data_dry/strategies/utils
+          elif /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl status freqtrade_dry | grep -q "RUNNING"; then
+            /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl stop freqtrade_dry
+          fi
+
+          if ! /mnt/disks/deeplearning/usr/bin/docker exec mydb [ -d "/home/runner/data_live" ]; then
+            /mnt/disks/deeplearning/usr/bin/docker exec mydb freqtrade create-userdir --userdir /home/runner/data_live
+            /mnt/disks/deeplearning/usr/bin/docker exec mydb mkdir -p /home/runner/data_live/strategies/utils
+          elif /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl status freqtrade_live | grep -q "RUNNING"; then
+            /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl stop freqtrade_live
+            /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl stop monitor_freqtrade
+          fi
+
+        fi
+  
+      exit 0
     fi
 
-    if ! /mnt/disks/deeplearning/usr/bin/docker exec mydb [ -d "/home/runner/data_live" ]; then
-      /mnt/disks/deeplearning/usr/bin/docker exec mydb freqtrade create-userdir --userdir /home/runner/data_live
-      /mnt/disks/deeplearning/usr/bin/docker exec mydb mkdir -p /home/runner/data_live/strategies/utils
-    elif /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl status freqtrade_live | grep -q "RUNNING"; then
-      /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl stop freqtrade_live
-      /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl stop monitor_freqtrade
+    if [ $i -lt $max_retries ]; then
+        sleep $interval
     fi
+  done
 
-  fi
+  echo "Condition not fulfilled after $max_retries checks ❌"
+  gh workflow run "main.yml" --repo "$REPO_NAME"
+  
 fi
 
