49d02ca7d320ed43ef722b1ac019cbdca233f890
eQuantum <8466209+eq19@users.noreply.github.com>
2025-09-04 13:20:49 +0700

    Update gcp.sh

--- .github/entrypoint/gcp.sh
+++ .github/entrypoint/gcp.sh
@@ -2,11 +2,17 @@
 
 # Max retries
 max_retries=10
-# Interval between checks (60s / 10 = 6s each)
-interval=6
+# Interval between checks (600s / 10 = 60s each)
+interval=60
 
 for ((i=1; i<=max_retries; i++)); do
+
     echo "Check $i of $max_retries..."
+    HEADER="Accept: application/vnd.github+json"
+    RESPONSE=$(gh api -H "${HEADER}" repos/${{ env.REPO_NAME }}/actions/runners)
+    TOTAL_COUNT=$(gh api -H "${HEADER}" /repos/${{ env.REPO_NAME }}/actions/runners --jq '.total_count')
+    STATUS=$(echo "$RESPONSE" | jq -r --arg NAME "${{ env.RUNNER_TITLE }}" '.runners[] | select(.name == $NAME).status')
+    #if [[ "$TOTAL_COUNT" -eq 0 ]] || [[ "$STATUS" == "offline" ]]; then gh workflow run "main.yml" --repo ${{ env.REPO_NAME }}; fi
 
     # Replace this with your condition
     if some_command_or_condition; then
