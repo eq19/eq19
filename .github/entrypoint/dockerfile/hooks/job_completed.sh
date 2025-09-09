82f50e80ae11384eb5d2ee648cb435690a5edeb3
eQuantum <8466209+eq19@users.noreply.github.com>
2025-09-09 09:32:28 +0700

    Update job_completed.sh

--- .github/entrypoint/dockerfile/hooks/job_completed.sh
+++ .github/entrypoint/dockerfile/hooks/job_completed.sh
@@ -38,6 +38,7 @@ if [ -d /mnt/disks/deeplearning/usr/local/sbin ]; then
   if [[ "$RERUN_RUNNER" == "true" ]]; then
     #$DOCKER exec mydb supervisorctl start freqtrade_dry
     $DOCKER exec mydb supervisorctl start freqtrade_live
+    $DOCKER exec mydb supervisorctl start monitor_freqtrade
     $DOCKER exec mydb service cron start
 
   #Check if ✅ $APP is running inside $CONTAINER
@@ -53,10 +54,11 @@ if [ -d /mnt/disks/deeplearning/usr/local/sbin ]; then
   else
     # Optionally restart:
     # docker start "$CONTAINER" && docker exec "$CONTAINER" supervisorctl start "$APP"
+    echo "❌ $APP is NOT running (either container is down or process crashed)."
     #$DOCKER exec mydb supervisorctl start freqtrade_dry
     $DOCKER exec mydb supervisorctl start freqtrade_live
+    $DOCKER exec mydb supervisorctl start monitor_freqtrade
     $DOCKER exec mydb service cron start
-    #echo "❌ $APP is NOT running (either container is down or process crashed)."
     
   fi
 fi
