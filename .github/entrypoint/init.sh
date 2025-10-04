0151a1508e7030c6b041f5317328db965e0e5c3d
eQuantum <8466209+eq19@users.noreply.github.com>
2025-10-04 08:52:44 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -194,7 +194,9 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
   DOCKER="/mnt/disks/deeplearning/usr/bin/docker"
   BASE_URL="https://raw.githubusercontent.com/eq19/maps/$MAP_BRANCH/user_data"
   SIGNATURE=$(echo -n "$PARAMS" | openssl sha512 -hmac "$API_SECRET" | cut -d' ' -f2)
-  WALLET=$(curl -s -X POST -H "Key: $API_KEY" -H "Sign: $SIGNATURE" -d "method=$METHOD" -d "nonce=$NONCE" "https://indodax.com/tapi/" | jq '.return.balance.idr')
+  BALLANCE=$(curl -s -X POST -H "Key: $API_KEY" -H "Sign: $SIGNATURE" -d "method=$METHOD" -d "nonce=$NONCE" "https://indodax.com/tapi/")
+  WALLET=$(echo $BALLANCE | jq '.return.balance.idr')
+  echo $WALLET
 
   for DIR_PATH in "${DIRS[@]}"; do
     for REL_PATH in "${FILES[@]}"; do
