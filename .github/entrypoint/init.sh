e3fbaaf13d09c7101b2cb66fd32489c1bd690459
eQuantum <8466209+eq19@users.noreply.github.com>
2025-12-28 21:13:36 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -349,6 +349,7 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
     HYPEROPT_PARAM="/home/runner/data_dry/strategies/hyperopt_params.json"
     ARTIFACT="/home/runner/data_dry/ft_client/test_client/results/orgs.json"
 
+    $DOCKER exec mydb rm "$HYPEROPT_PARAM"
     $DOCKER exec mydb bash -c 'python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_dry "${ID}" "${PARAM:-nil}" "${EPOCHS:-100}"'
     $DOCKER exec mydb bash -c "curl -s -X POST -H 'Authorization: Bearer $TOKEN' -H 'Content-Type: application/json' https://us-central1-marketleader.cloudfunctions.net/function --data @'$ARTIFACT' | jq '.' > '$HYPEROPT_PARAM'"
     $DOCKER exec mydb cat "$HYPEROPT_PARAM"
@@ -393,6 +394,7 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
     HYPEROPT_PARAM="/home/runner/data_dry/strategies/hyperopt_params.json"
     ARTIFACT="/home/runner/data_dry/ft_client/test_client/results/orgs.json"
 
+    $DOCKER exec mydb rm "$HYPEROPT_PARAM"
     $DOCKER exec mydb bash -c 'python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_dry "${ID}" "${PARAM:-nil}" "${EPOCHS:-100}"'
     $DOCKER exec mydb bash -c "curl -s -X POST -H 'Authorization: Bearer $TOKEN' -H 'Content-Type: application/json' https://us-central1-marketleader.cloudfunctions.net/function --data @'$ARTIFACT' | jq '.' > '$HYPEROPT_PARAM'"
     $DOCKER exec mydb cat "$HYPEROPT_PARAM"
