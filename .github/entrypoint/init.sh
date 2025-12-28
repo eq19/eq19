4bf459c93b1b3f19cfe8910e6cca9b174a03c54d
eQuantum <8466209+eq19@users.noreply.github.com>
2025-12-29 06:25:26 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -272,18 +272,18 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
     "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/JEKYLL_CONFIG" \
     | jq -r '.value' > _config.yml
 
-     $DOCKER exec mydb bash -c \
-      "curl -s -H 'Authorization: token $GH_TOKEN' -H 'Accept: application/vnd.github.v3+json' \
-      https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/ORGS_JSON \
-      | jq -r '.value' > /home/runner/data_dry/ft_client/test_client/results/orgs.json"
-    $DOCKER exec mydb bash -c \
-      "curl -s -H 'Authorization: token $GH_TOKEN' -H 'Accept: application/vnd.github.v3+json' \
-      https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/PARAMS_DRY \
-      | jq -r '.value' > /home/runner/data_dry/strategies/fibbo.json"
-    $DOCKER exec mydb bash -c \
-      "curl -s -H 'Authorization: token $GH_TOKEN' -H 'Accept: application/vnd.github.v3+json' \
-      https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/PARAMS_LIVE \
-      | jq -r '.value' > /home/runner/data_live/strategies/fibbo.json"
+  $DOCKER exec mydb bash -c \
+    "curl -s -H 'Authorization: token $GH_TOKEN' -H 'Accept: application/vnd.github.v3+json' \
+    https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/ORGS_JSON \
+    | jq -r '.value' > /home/runner/data_dry/ft_client/test_client/results/orgs.json"
+  $DOCKER exec mydb bash -c \
+    "curl -s -H 'Authorization: token $GH_TOKEN' -H 'Accept: application/vnd.github.v3+json' \
+    https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/PARAMS_DRY \
+    | jq -r '.value' > /home/runner/data_dry/strategies/fibbo.json"
+  $DOCKER exec mydb bash -c \
+    "curl -s -H 'Authorization: token $GH_TOKEN' -H 'Accept: application/vnd.github.v3+json' \
+    https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/PARAMS_LIVE \
+    | jq -r '.value' > /home/runner/data_live/strategies/fibbo.json"
 
   # Case on rerun self host runner 
   if [[ "$RERUN_RUNNER" == "true" ]]; then
