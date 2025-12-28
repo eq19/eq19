c2db31c48a5d633f1b36353da83a193e0b8cd0f8
eQuantum <8466209+eq19@users.noreply.github.com>
2025-12-28 09:33:28 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -306,13 +306,10 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
       "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/JEKYLL_CONFIG" \
       | jq -r '.value' > _config.yml
 
-    ID=$(sed -n 's/^id: //p' _config.yml)
-    $DOCKER exec mydb cat $HYPEROPT_PARAM
-    $DOCKER exec mydb cp $HYPEROPT_PARAM /home/runner/data_dry/strategies/hyperopt_params.json
-    $DOCKER exec mydb cp $HYPEROPT_PARAM /home/runner/data_live/strategies/hyperopt_params.json
-    #$DOCKER exec mydb bash -c "python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_dry ${ID} ${PARAM:-'nil'} ${EPOCHS:-100}"
-    #$DOCKER exec mydb bash -c "python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_live ${ID} ${{ env.PARAM || 'nil' }} ${{ env.EPOCHS || 100 }}"
-    #bash /home/runner/user_data/ft_client/test_client/maps.sh
+    ID=$(sed -n 's/^id:[[:space:]]*//p' _config.yml)
+    $DOCKER exec mydb bash -c 'python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_dry "${ID}" "${PARAM:-nil}" "${EPOCHS:-100}"'
+    $DOCKER exec mydb bash -c 'python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_live "${ID}" "${PARAM:-nil}" "${EPOCHS:-100}"'
+    $DOCKER exec mydb bash /home/runner/user_data/ft_client/test_client/maps.sh
 
   # Case Dry-run is better than live mode
   elif [[ "$RERUN_RUNNER" == "false" ]] && \
@@ -340,8 +337,18 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
       https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/PARAMS_DRY \
       | jq -r '.value' > /home/runner/data_dry/strategies/fibbo.json"
 
-    $DOCKER exec mydb bash -c "python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_dry ${{ env.ID }} ${{ env.PARAM || 'nil' }} ${{ env.EPOCHS || 100 }}"
+    # Get the config value and save to file.json
+    curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
+      "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/ORGS_JSON" \
+      | jq -r '.value' > _data/orgs.json
+    curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
+      "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/JEKYLL_CONFIG" \
+      | jq -r '.value' > _config.yml
+
+    ID=$(sed -n 's/^id:[[:space:]]*//p' _config.yml)
+    $DOCKER exec mydb bash -c 'python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_dry "${ID}" "${PARAM:-nil}" "${EPOCHS:-100}"'
     $DOCKER exec mydb bash /home/runner/user_data/ft_client/test_client/maps.sh
+    $DOCKER exec mydb cat /home/runner/data_dry/strategies/hyperopt_params.json
 
   # Case Live mode is better than dry-run
   elif [[ "$RERUN_RUNNER" == "false" ]] && \
@@ -359,8 +366,18 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
       https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/PARAMS_DRY \
       | jq -r '.value' > /home/runner/data_dry/strategies/fibbo.json"
 
-    $DOCKER exec mydb bash -c "python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_dry ${{ env.ID }} ${{ env.PARAM || 'nil' }} ${{ env.EPOCHS || 100 }}"
+    # Get the config value and save to file.json
+    curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
+      "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/ORGS_JSON" \
+      | jq -r '.value' > _data/orgs.json
+    curl -s -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" \
+      "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/variables/JEKYLL_CONFIG" \
+      | jq -r '.value' > _config.yml
+
+    ID=$(sed -n 's/^id:[[:space:]]*//p' _config.yml)
+    $DOCKER exec mydb bash -c 'python /home/runner/user_data/ft_client/test_client/app.py /home/runner/data_dry "${ID}" "${PARAM:-nil}" "${EPOCHS:-100}"'
     $DOCKER exec mydb bash /home/runner/user_data/ft_client/test_client/maps.sh
+    $DOCKER exec mydb cat /home/runner/data_dry/strategies/hyperopt_params.json
 
   fi
 
