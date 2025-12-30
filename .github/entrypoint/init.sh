b0fe902fae986e9e584d1e4cdc3445044f95edcc
eQuantum <8466209+eq19@users.noreply.github.com>
2025-12-30 23:50:29 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -293,9 +293,7 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
   CONFIG_BASIC="$BASE_URL/config_examples/config_basic.example.json"
   CONFIG_PAIRLIST="$BASE_URL/config_examples/config_pairlist.example.json"
   CONFIG_EXCHANGE="$BASE_URL/config_examples/config_exchange.example.json"
-  HYPEROPT_PARAM="/home/runner/user_data/strategies/hyperopt_params.json"
   EXCHANGE_PARAM="/home/runner/data_live/config_examples/config_exchange.example.json"
-  PAIRLIST_PARAM="/home/runner/user_data/config_examples/config_pairlist.example.json"
 
   # Strict handling
   set -euo pipefail
