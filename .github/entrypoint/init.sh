192836734bf8dfab7a7964732233a66a93634c30
eQuantum <8466209+eq19@users.noreply.github.com>
2025-12-30 15:55:17 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -230,9 +230,11 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
   BEARER=$($GCLOUD auth print-identity-token --audiences=https://us-central1-marketleader.cloudfunctions.net/function)
 
   for idx in "${!DIRS[@]}"; do
-    echo "Folder: ${DIRS[$idx]} → Params: ${PARAMS[$idx]}"
-    ARTIFACT="/home/runner/${DIRS[$idx]}/ft_client/test_client/results/orgs.json"
+    PARAM_NAME="${PARAMS[$idx]}"
+    DIR_PATH="/home/runner/${DIR[$idx]}"
+    ARTIFACT="${DIR_PATH}/ft_client/test_client/results/orgs.json"
     $DOCKER exec mydb mkdir -p "$(dirname "$ARTIFACT")"
+    echo "Folder: ${DIR_PATH} → Params: ${PARAM_NAME}"
 
     $DOCKER exec mydb bash -c \
       "curl -s -H 'Authorization: token $GH_TOKEN' -H 'Accept: application/vnd.github.v3+json' \
@@ -240,11 +242,11 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
       | jq -r '.value' > $ARTIFACT"
     $DOCKER exec mydb bash -c \
       "curl -s -H 'Authorization: token $GH_TOKEN' -H 'Accept: application/vnd.github.v3+json' \
-      https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/${PARAMS[$idx]} \
-      | jq -r '.value' > /home/runner/${DIRS[$idx]}/strategies/fibbo.json"
+      https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/${PARAM_NAME} \
+      | jq -r '.value' > ${DIR_PATH}/strategies/fibbo.json"
 
-    HYPEROPT_PARAM="/home/runner/${DIRS[$idx]}/strategies/hyperopt_params.json"
-    $DOCKER exec mydb bash -c 'python /home/runner/user_data/ft_client/test_client/app.py "/home/runner/${DIRS[$idx]}" "${ID:-1}" "${PARAM:-nil}" "${EPOCHS:-100}"'
+    HYPEROPT_PARAM="${DIR_PATH}/strategies/hyperopt_params.json"
+    $DOCKER exec mydb bash -c "python /home/runner/user_data/ft_client/test_client/app.py \"$DIR_PATH\" \"${ID:-1}\" \"${PARAM_NAME:-nil}\" \"${EPOCHS:-100}\""
     $DOCKER exec mydb bash -c "curl -s -X POST -H 'Authorization: Bearer $BEARER' -H 'Content-Type: application/json' https://us-central1-marketleader.cloudfunctions.net/function --data @'$ARTIFACT' | jq '.' > '$HYPEROPT_PARAM'"
 
     for REL_PATH in "${FILES[@]}"; do
