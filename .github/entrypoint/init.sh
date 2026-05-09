78489d7077e65806ddf3ddf87a21dbed66dabe37
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-09 13:19:33 +0700

    Fix chmod command for downloaded shell scripts

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -373,7 +373,7 @@ fi
     
         if $DOCKER exec mydb curl -sf -o "$DEST_PATH" "$DOWNLOAD_URL"; then
           if $DOCKER exec mydb test -s "$DEST_PATH"; then
-            [[ "$DEST_PATH" == *.sh ]] && chmod +x "$DEST_PATH"
+            [[ "$DEST_PATH" == *.sh ]] && $DOCKER exec mydb chmod +x "$DEST_PATH"
             echo "✅ [SUCCESS] Downloaded: $DEST_PATH"
             break
           else
