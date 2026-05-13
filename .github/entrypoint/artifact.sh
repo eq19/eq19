2f64332b6212196fcf3578145359e5ebe0dc9323
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-14 03:52:29 +0700

    Refactor SITEID calculation in artifact.sh

--- .github/entrypoint/artifact.sh
+++ .github/entrypoint/artifact.sh
@@ -90,15 +90,12 @@ jekyll_build() {
   # the id's is square of 13 orgs dust 169 repos
   if [[ $1 == "Chetabahana.github.io" ]]; then
     SITEID=1
-  # the 7 units of user is not part of orgs
-  #elif [[ $1 == "eq19.github.io" ]]; then
-    #SITEID=30
   else
     SITEID=$(( $3 + 2 ))
   fi
 
   if  [[ "${OWNER}" == "eq19" ]]; then
-    SITEID=$(( $3 + 2 - 140 ))
+    SITEID=$(( $SITEID - 140 ))
     sed -i "1s|^|description: An endeavor to discover the Final Theory\n\n|" ${RUNNER_TEMP}/_config.yml
   else
     DESCRIPTION=$(gh api -H "${HEADER}" /orgs/${OWNER} --jq '.description')
