121103f9847cbb0619ecac4fcd42775fe7e98368
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-14 21:35:09 +0700

    Update FREQAI_MODEL in init.sh for dry run

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -340,6 +340,7 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
       $DOCKER exec mydb sed -i "s|tradesv3|tradesv3_dry|g" $CONFIG_DRY
       $DOCKER exec mydb mkdir -p "$(dirname "$EXCHANGE_DRY")"
       $DOCKER exec mydb curl -sf -o "$EXCHANGE_DRY" "$CONFIG_EXCHANGE"
+      $DOCKER exec mydb sed -i 's|RUN_MODE="dry",FREQAI_MODEL="[^\"]*"|RUN_MODE="dry",FREQAI_MODEL="$FREQAIMODEL_DRY"|g' $CONF
       $DOCKER exec mydb bash -c "jq --argjson pairs '$PAIRS' '.exchange.pair_whitelist = \$pairs' '$EXCHANGE_DRY' > config.tmp && mv config.tmp '$EXCHANGE_DRY'"
    fi 
 fi
