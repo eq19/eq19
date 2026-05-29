9ecc3fe57346d56fef0446bce0472909c31a4a94
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-29 23:18:28 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -366,7 +366,7 @@ fi
       https://api.github.com/repos/$GITHUB_REPOSITORY/actions/variables/${PARAM_NAME} \
       | jq -r '.value' > ${DIR_PATH}/strategies/fibbo.json"
 
-    $DOCKER exec mydb bash -c "bash /home/runner/user_data/ft_client/test_client/maps.sh $APP_PATH $DIR_PATH $ARTIFACT $HYPEROPT_PARAM="
+    $DOCKER exec mydb bash -c "bash /home/runner/user_data/ft_client/test_client/maps.sh $APP_PATH $DIR_PATH $ARTIFACT $HYPEROPT_PARAM"
     #$DOCKER exec mydb bash -c "python /home/runner/user_data/ft_client/test_client/app.py \"$DIR_PATH\" \"${ID:-1}\" \"${PARAM_NAME:-nil}\" \"${EPOCHS:-100}\""
     #$DOCKER exec mydb bash -c "curl -s -X POST -H 'Authorization: Bearer $BEARER' -H 'Content-Type: application/json' https://us-central1-marketleader.cloudfunctions.net/function --data @'$ARTIFACT' | jq '.' > '$HYPEROPT_PARAM'"
 
