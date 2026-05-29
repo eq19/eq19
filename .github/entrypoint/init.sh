b6458dc10d0ec6a303a161223575c08541828192
eQuantum <8466209+eq19@users.noreply.github.com>
2026-05-29 19:38:08 +0700

    Update init.sh

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -350,6 +350,7 @@ fi
   for idx in "${!DIRS[@]}"; do
     PARAM_NAME="${PARAMS[$idx]}"
     DIR_PATH="/home/runner/${DIRS[$idx]}"
+    APP_PATH="/home/runner/user_data/ft_client/test_client/app.py"
     ARTIFACT="${DIR_PATH}/ft_client/test_client/results/orgs.json"
     $DOCKER exec mydb mkdir -p "$(dirname "$ARTIFACT")"
     echo -e "$hr\nFolder: ${DIR_PATH} → Params: ${PARAM_NAME}"
@@ -364,7 +365,7 @@ fi
       | jq -r '.value' > ${DIR_PATH}/strategies/fibbo.json"
 
     HYPEROPT_PARAM="${DIR_PATH}/strategies/hyperopt_params.json"
-    $DOCKER exec mydb bash -c "bash /home/runner/user_data/ft_client/test_client/maps.sh"
+    $DOCKER exec mydb bash -c "bash /home/runner/user_data/ft_client/test_client/maps.sh $APP_PATH $DIR_PATH"
     #$DOCKER exec mydb bash -c "python /home/runner/user_data/ft_client/test_client/app.py \"$DIR_PATH\" \"${ID:-1}\" \"${PARAM_NAME:-nil}\" \"${EPOCHS:-100}\""
     #$DOCKER exec mydb bash -c "curl -s -X POST -H 'Authorization: Bearer $BEARER' -H 'Content-Type: application/json' https://us-central1-marketleader.cloudfunctions.net/function --data @'$ARTIFACT' | jq '.' > '$HYPEROPT_PARAM'"
 
